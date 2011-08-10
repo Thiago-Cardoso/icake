99e655ff02da82a03cd2ab7a0e7779cf36aa5d47
Adriano C. de Moura <adrianodemoura@gmail.com>
2011-08-10 15:41:04 -0300

    Correção no cadastro de usuários

--- config/core.php
+++ config/core.php
@@ -287,18 +287,17 @@
  *
  *
  * Memcache (http://www.danga.com/memcached/)
- *
- * 	 Cache::config('default', array(
- *		'engine' => 'Memcache', //[required]
- *		'duration'=> 3600, //[optional]
- *		'probability'=> 100, //[optional]
- * 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
- * 		'servers' => array(
- * 			'127.0.0.1:11211' // localhost, default port 11211
- * 		), //[optional]
- * 		'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
- * 		'persistent' => true, // [optional] set this to false for non-persistent connections
- *	));
- *
  */
-	Cache::config('default', array('engine' => 'File'));
+Cache::config('default', array(
+		'engine' => 'Memcache', //[required]
+		'duration'=> 3600, //[optional]
+		'probability'=> 100, //[optional]
+ 		'prefix' => Inflector::slug(APP_DIR) . '_', //[optional]  prefix every cache file with this string
+ 		'servers' => array(
+ 			'127.0.0.1:11211' // localhost, default port 11211
+ 		), //[optional]
+ 		'compress' => false, // [optional] compress data in Memcache (slower, but uses less memory)
+ 		'persistent' => true, // [optional] set this to false for non-persistent connections
+	));
+
+/*Cache::config('default', array('engine' => 'File'));*/
