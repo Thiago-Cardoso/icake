637cffbd2b6e0e870b3d54fa8fe758cb3c0b2031
Adriano C. de Moura <adrianodemoura@gmail.com>
2011-09-07 18:16:41 -0300

    Correção no model de clientes e também correção no helper visao

--- plugins/jcake/views/helpers/visao.php
+++ plugins/jcake/views/helpers/visao.php
@@ -440,10 +440,12 @@ class VisaoHelper extends Helper {
 			case 'cpf':
 			case '999.999.999-99':
 				$mascarado = substr($valor,0,3).'.'.substr($valor,3,3).'.'.substr($valor,6,3).'-'.substr($valor,9,2);
+				if ($valor=='0') $mascarado = '';
 				break;
 			case 'cnpj':
 			case '99.999.999/9999-99':
 				$mascarado = substr($valor,0,2).'.'.substr($valor,2,3).'.'.substr($valor,5,3).'/'.substr($valor,8,4).'-'.substr($valor,12,2);
+				if ($valor=='0') $mascarado = '';
 				break;
 			case 'aniversario':
 			case '99/99':
