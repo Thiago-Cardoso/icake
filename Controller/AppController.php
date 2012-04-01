8d9e82dae0cdbfcb2a7431f445be532532dcc6d0
Adriano Carneiro de Moura <adrianodemoura@gmail.com>
2012-04-01 17:08:03 -0300

    enviaando url anterior, pra visão.

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -99,6 +99,12 @@ class AppController extends Controller {
 				);
 				unset($Usuario);
 			}
+		
+			// variáveis globais para todas as vies
+			$urlVolta		= $this->referer();
+			$meses 			= array(1=>'Janeiro', 2=>'Fevereiro', 3=>'Março', 4=>'Abril', 5=>'Maio', 6=>'Junho', 7=>'Julho', 8=>'Agosto', 9=>'Setembro', 10=>'Outubro', 11=>'Novembro', 12=>'Dezembro');
+			$this->set(compact('meses','urlVolta'));
+			
 		}
 
 		// descobrindo o schema do model corrente
